739b41bd5a0a93883ed9adf8c02a6fa037ba7e28
johnj974 <jjdug@hotmail.com>
2020-01-23 12:23:55 +0000

    footer links added

---
+++ assets/js/weather.js
@@ -0,0 +1,16 @@
+//   2a1b20e296836689a2296c960cf5a29a   api key
+//   api.openweathermap.org/data/2.5/weather?q=dublin&units=metric&APPID=2a1b20e296836689a2296c960cf5a29a
+
+$.getJSON("https://api.openweathermap.org/data/2.5/weather?q=dublin&units=metric&APPID=2a1b20e296836689a2296c960cf5a29a",function(data){
+    console.log(data)
+
+    let icon = "http://api.openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+    let weather = data.weather[0].main;
+    let temp = Math.floor(data.main.temp);
+    
+
+    $(".icon").attr("src",icon);
+    $(".weather").append(weather);
+    $(".temp").append(temp);
+    
+});
