b06c85faa6729c6eebea978deea8f5572a4f29f5
johnj974 <jjdug@hotmail.com>
2020-01-23 08:24:39 +0000

    starting over

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,4 @@
 
 
-var xhr = new XMLHttpRequest();
-
-xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("api").innerHTML = this.responseText;
-    }
-};
-
-xhr.open("GET", "http://api.weatherstack.com/");
-
-xhr.send();
+var a = "can you hear me"
+console.log(a);
