4b3a90aeeca64fdc675373a591cc9e7be828394b
johnj974 <jjdug@hotmail.com>
2020-01-28 10:13:56 +0000

    more updates to country selector

--- assets/js/countries.js
+++ assets/js/countries.js
@@ -5,19 +5,20 @@
 function otherFunction(countryname){$.getJSON("https://restcountries.eu/rest/v2/name/" + countryname,function(data){
     
 
-    let flag = data[0].flag;
+    //let flag = data[0].flag;
     let capital = data[0].capital;
     let population = data[0].population;
     let language = data[0].languages[0].nativeName;
     let region = data[0].region;
 
-    $(".flag").attr(flag)
+    document.querySelector("#flag img").src = data[0].flag;
+    //$(".flag").attr(flag)
     $(".capital").append(` ${capital}`)
     $(".population").append(` ${population}`)
     $(".language").append(` ${language}`)
     $(".region").append(` ${region}`)
 
-
+    
 
     
     
