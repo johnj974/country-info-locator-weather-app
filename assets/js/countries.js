bfdca2c03bba2fb2da149876bf77bb06955a5672
johnj974 <jjdug@hotmail.com>
2020-01-29 11:57:03 +0000

    scroll to results

--- assets/js/countries.js
+++ assets/js/countries.js
@@ -10,19 +10,20 @@ function otherFunction(countryname){$.getJSON("https://restcountries.eu/rest/v2/
     let population = data[0].population;
     let language = data[0].languages[0].nativeName;
     let region = data[0].region;
+    
 
     document.querySelector("#flag img").src = data[0].flag;
     $(".capital").append(` ${capital}`)
     $(".population").append(` ${population}`)
     $(".language").append(` ${language}`)
-    $(".region").append(` ${region}`)
-
-    
-
-    
-    
+    $(".region").append(` ${region}`)   
 
    
 })}
 
-    
+    $("#country").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#results").offset().top
+    }, 2000);
+});
+      
